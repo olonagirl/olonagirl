4cb8d6a8d3a66f6e30e6da3c4367e0ae2d388443
Samson Okunola <smsnmicheal@gmail.com>
2023-12-17 23:08:53 +0100

    changes - changed node versioon in `package.json` to 18.x - added API key in code !unsafe-inline!

--- app/_lib/commerce.ts
+++ app/_lib/commerce.ts
@@ -1,6 +1,8 @@
 import Commerce from "@chec/commerce.js"
 
 export const commerce = new Commerce(
-	String(process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY),
+	// !UNSAFE - DO NOT USE IN PRODUCTION
+	// TODO: Replace with Chec public key from environment variables
+	"pk_test_4198275a30b8c87a6043c2e351d8a78b422cfc1a5a9ef",
 	true
 )
