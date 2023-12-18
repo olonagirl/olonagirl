cc475026ec4b753d69bddc667de290cdfb624e87
olonagirl <153048499+olonagirl@users.noreply.github.com>
2023-12-18 17:01:12 +0100

    fixed deployment error

--- app/account/user/page.tsx
+++ app/account/user/page.tsx
@@ -1,10 +1,4 @@
-import { commerce } from "@/app/_lib/commerce"
-
-const User = async () => {
-	const customer = await commerce.customer.about()
-
-	if (!customer) return null
-
+const User = () => {
 	return (
 		<main className="w-full px-5 py-10 lg:px-20">
 			<section className="w-full"></section>
