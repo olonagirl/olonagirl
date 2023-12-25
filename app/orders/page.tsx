90189786b03f64f196caa8d28dbd720f7e0e7e0c
Samson Okunola <smsnmicheal@gmail.com>
2023-12-25 19:11:27 +0100

    fix for deployment errors

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,26 +1,15 @@
 import React from "react"
 
-import { commerce } from "../_lib/commerce"
-import { Loader } from "../_components"
-import { store } from "../_store"
+// import { commerce } from "../_lib/commerce"
+// import { Loader } from "../_components"
+// import { store } from "../_store"
 
 const Page = async () => {
-	const { user } = store()
-	const orders = await commerce.customer.getOrders(user?.id)
-
-	if (!orders) return <Loader />
-
 	return (
 		<main className="w-full px-5 py-10 lg:px-20">
 			<p className="my-4 text-2xl lg:text-4xl">Orders</p>
 			<hr className="my-4 w-full bg-dark" />
-			<section className="my-5 flex w-full flex-col gap-4 lg:w-1/2">
-				{orders.data.map((order) => (
-					<div key={order.id} className="flex w-full flex-col border p-4">
-						<p className="text-2xl">Order {order.id}</p>
-					</div>
-				))}
-			</section>
+			<section className="my-5 flex w-full flex-col gap-4 lg:w-1/2"></section>
 		</main>
 	)
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,34 @@
-import React from "react"
+"use client"
+import { useEffect, useState } from "react"
+import Confetti from "react-confetti"
 
 const Success = () => {
-	return <main className="w-full px-5 py-10 lg:px-20"></main>
+	const [dimension, setDimension] = useState({
+		height: typeof window !== "undefined" ? window.innerHeight : 900,
+		width: typeof window !== "undefined" ? window.innerWidth : 1440,
+	})
+
+	const handleWidthResize = () => {
+		if (typeof window !== "undefined") {
+			setDimension({ height: window.innerHeight, width: window.innerWidth })
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener("resize", handleWidthResize)
+		return () => window.removeEventListener("resize", handleWidthResize)
+	}, [])
+
+	return (
+		<main className="w-full px-5 py-10 lg:px-20">
+			<Confetti
+				height={dimension.height}
+				width={dimension.width}
+				numberOfPieces={5000}
+				recycle={false}
+			/>
+		</main>
+	)
 }
 
 export default Success
