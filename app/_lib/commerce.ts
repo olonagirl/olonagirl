16c9fac4b51c83ea79e8fc1d7fe5ce7e648c7938
Samson Okunola <smsnmicheal@gmail.com>
2024-03-05 15:35:38 +0100

    chore; build fixes

--- app/_lib/commerce.ts
+++ app/_lib/commerce.ts
@@ -1,6 +1,5 @@
 import Commerce from "@chec/commerce.js"
 
-const apiKey =
-	process.env.CHEC_PUBLIC_KEY ?? process.env.NEXT_PUBLIC_CHEC_PUBLIC_KEY
+const apiKey = "pk_55251340e2e1c2d6b6c6b4c9a716aaaa7327c64feba2c"
 
 export const commerce = new Commerce(apiKey, true)
