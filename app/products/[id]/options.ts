68b95dac4692c4132748cedc7e5ce7e1c3bb226b
Samson Okunola <smsnmicheal@gmail.com>
2024-03-08 21:43:20 +0100

    chore: added fabric care

---
+++ app/products/[id]/options.ts
@@ -0,0 +1,22 @@
+export const FabricCare = [
+	{
+		fabric: "Adire Fabric",
+		care: [
+			"Handwash only",
+			"Use mild bar soap or mild detergent to wash",
+			"Do not dry under direct sunlight as this will cause colours to fade",
+			"Air dry in a cool area",
+			"Steam iron clothing for best finish",
+		],
+	},
+	{
+		fabric: "Ankara Fabric",
+		care: [
+			"It is best to separate colours while washing",
+			"Handwash or machine wash with mild detergent",
+			"Wash in only room temperature water",
+			"Do not use lukewarm or hot water as this will cause the fabric colour to fade",
+			"Steam iron fabric to give best look",
+		],
+	},
+]
